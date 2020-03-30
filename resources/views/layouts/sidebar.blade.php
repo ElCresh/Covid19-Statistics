4b9587f30eb41bd0dc161777701a777b5c18829b
Andrea Crescentini <ac@andreacrescentini.com>
2020-03-30 14:00:23 +0200

    Added data sources on sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -37,6 +37,21 @@
                         </p>
                     </a>
                 </li>
+                <li class="nav-header">Fonti dati</li>
+                <li class="nav-item">
+                    <a href="https://github.com/pcm-dpc/COVID-19" target="_blank" class="nav-link">
+                        <i class="nav-icon fas fa-database"></i>
+                        <p>
+                            Protezione Civile Italiana
+                        </p>
+                    </a>
+                    <a href="https://data.europa.eu/euodp/it/data/dataset/covid-19-coronavirus-data/resource/260bbbde-2316-40eb-aec3-7cd7bfc2f590" target="_blank" class="nav-link">
+                        <i class="nav-icon fas fa-database"></i>
+                        <p>
+                            OpenData UE
+                        </p>
+                    </a>
+                </li>
                 <li class="nav-header">Altro</li>
                 <li class="nav-item">
                     <a href="https://github.com/ElCresh/Covid19-Statistics" target="_blank" class="nav-link">
