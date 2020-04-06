07e01afaceefdfa72df319d21d7b572c88eee358
Andrea Crescentini <ac@andreacrescentini.com>
2020-04-06 20:23:36 +0200

    Updated meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="keywords" content="covid19,statistics,statistiche">
-    <meta name="description" content="Visualizzazione dati e grafici provenienti dai dati raccolti dalla Protezione Civile Italiana">
+    <meta name="keywords" content="covid19,statistics,statistiche,notiosoft,protezione,civile,je,sse,italia,san marino">
+    <meta name="description" content="Dashboard di aggregazione dati su COVID-19 provanienti dal tutto il mondo">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
